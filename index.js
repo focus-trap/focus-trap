6d953a70fba48878cec02922a90bb9a66ed5ab50
Daniel Walmsley <goldsounds@gmail.com>
2016-01-25 11:25:40 -0800

    Fix crash on IE11 when previouslyFocused is null

--- index.js
+++ index.js
@@ -59,7 +59,9 @@ function deactivate() {
   if (config.onDeactivate) config.onDeactivate();
 
   setTimeout(function() {
-    previouslyFocused.focus();
+    if ( previouslyFocused ) {
+      previouslyFocused.focus();  
+    }
   }, 0);
 }
 
