4bc72f0af01fd18f63c69b9025df310f1a1e4444
Stefan Cameron <stefan@stefcameron.com>
2020-11-16 14:48:05 -0600

    Fix/enhance demo styling

--- demo/js/multiple-elements.js
+++ demo/js/multiple-elements.js
@@ -1,19 +1,24 @@
 var { createFocusTrap } = require('../../dist/focus-trap');
 
 var container = document.getElementById('multipleelements');
+var selectors = ['#multipleelements-1', '#multipleelements-3'];
 
-var focusTrap = createFocusTrap(
-  ['#multipleelements-1', '#multipleelements-3'],
-  {
-    clickOutsideDeactivates: true,
-    onActivate: function () {
-      container.className = 'trap is-active';
-    },
-    onDeactivate: function () {
-      container.className = 'trap';
-    },
-  }
-);
+var focusTrap = createFocusTrap(selectors, {
+  clickOutsideDeactivates: true,
+  onActivate: function () {
+    container.className = 'trap is-active';
+    selectors.forEach(
+      (selector) =>
+        (document.querySelector(selector).className = 'is-active-nested')
+    );
+  },
+  onDeactivate: function () {
+    container.className = 'trap';
+    selectors.forEach(
+      (selector) => (document.querySelector(selector).className = null)
+    );
+  },
+});
 
 document
   .getElementById('activate-multipleelements')
