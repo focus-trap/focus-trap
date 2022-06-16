c9287928617ea5919c6c08e7285fa26a30958c25
Stefan Cameron <stefan@stefcameron.com>
2022-06-16 17:54:48 -0500

    Clean up

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const setupPlugins = require('./cypress/plugins/index.js');
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return setupPlugins(on, config);
     },
