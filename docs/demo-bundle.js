dee5ac258f121605ec50ba80696aa482eeb703e7
Stefan Cameron <stefan@stefcameron.com>
2023-03-29 11:08:40 -0500

    Fix new lint errors

--- docs/demo-bundle.js
+++ docs/demo-bundle.js
@@ -1,13 +1,12 @@
 /*!
-* focus-trap 7.3.1
+* focus-trap 7.4.0
 * @license MIT, https://github.com/focus-trap/focus-trap/blob/master/LICENSE
 */
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':9967/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 var focusTrapDemoBundle = (function () {
     'use strict';
 
     (function() {
-        const env = {"BUILD_ENV":"demo","IS_CYPRESS_ENV":""};
+        const env = {"BUILD_ENV":"demo","IS_CYPRESS_ENV":"chrome"};
         try {
             if (process) {
                 process.env = Object.assign({}, process.env);
